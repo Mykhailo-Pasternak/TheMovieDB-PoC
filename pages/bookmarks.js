4accac9a3af5de4ad36b7272d1b3d86876e15a38
MYKHAILO PASTERNAK <sscorpio2006@gmail.com>
2021-07-14 02:54:36 -0700

    deploy

--- pages/bookmarks.js
+++ pages/bookmarks.js
@@ -6,29 +6,32 @@ import { helper } from "../helper/helper.js";
 import { likeListener } from "../helper/listeners.js";
 
 export function bookmarks() {
-    const localFilms = window.localStorage.getItem("films");
-    if (localFilms === null) {
-        return;
+  const localFilms = window.localStorage.getItem("films");
+  if (localFilms === null) {
+    return;
+  }
+
+  const data = JSON.parse(localFilms);
+  header();
+  main();
+  helper();
+  likeListener();
+
+  if (localFilms !== null) {
+    const arrFilmsHTML = templateFilmsHTML(data);
+
+    render(arrFilmsHTML);
+  }
+  const listOfFilmsELEM = document.querySelector(".listOfFilms");
+  listOfFilmsELEM.addEventListener("click", (evt) => {
+    const filmElement = evt.target.closest("li");
+    const onImg = evt.target.closest("img");
+    if (onImg !== null) {
+      window.history.pushState(
+        null,
+        null,
+        `/movies/${filmElement.dataset.movie_id}`
+      );
     }
-
-    const data = JSON.parse(localFilms);
-    header();
-    main();
-    helper();
-
-    likeListener();
-
-    if (localFilms !== null) {
-        const arrFilmsHTML = templateFilmsHTML(data);
-
-        render(arrFilmsHTML);
-    }
-    const listOfFilmsELEM = document.querySelector(".listOfFilms");
-    listOfFilmsELEM.addEventListener("click", (evt) => {
-        const filmElement = evt.target.closest("li");
-        const onImg = evt.target.closest("img");
-        if (onImg !== null) {
-            window.history.pushState(null, null, `/movies/${filmElement.dataset.movie_id}`);
-        }
-    });
-};
+  });
+}
