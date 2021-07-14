caf078cc46614a55ac8d5410102b42bd40a6f525
MYKHAILO PASTERNAK <sscorpio2006@gmail.com>
2021-07-14 03:41:40 -0700

    search problem

--- index.js
+++ index.js
@@ -4,56 +4,61 @@ import { bookmarks } from "./pages/bookmarks.js";
 import { search } from "./pages/search.js";
 
 const routes = [
-    {
-        match: (url) => {
-            return url === "/";
-        },
-        renderRoute: popular
+  {
+    match: (url) => {
+      return url === "/";
     },
-    {
-        match: (url) => {
-            return url.includes("/movies");
-        },
-        renderRoute: movie
+    renderRoute: popular,
+  },
+  {
+    match: (url) => {
+      return url.includes("/movies");
     },
-    {
-        match: (url) => {
-            return url.includes("/bookmarks");
-        },
-        renderRoute: bookmarks
+    renderRoute: movie,
+  },
+  {
+    match: (url) => {
+      return url.includes("/bookmarks");
     },
-    {
-        match: (url) => {
-            return url.includes("/search");
-        },
-        renderRoute: search
-    }
+    renderRoute: bookmarks,
+  },
+  {
+    match: (url) => {
+      return url.includes("/search");
+    },
+    renderRoute: search,
+  },
 ];
 
 class Router {
-    constructor(routes) {
-        this._routes = routes;
+  constructor(routes) {
+    this._routes = routes;
 
-        this.reroute();
+    this.reroute();
 
-        window.history.pushState = (data, title, url) => {
-            History.prototype.pushState.apply(window.history, [data, title, url]);
-            this.reroute();
-        };
+    window.history.pushState = (data, title, url) => {
+      History.prototype.pushState.apply(window.history, [data, title, url]);
+      this.reroute();
+    };
 
-        window.onpopstate = () => {
-            this.reroute();
-        };
-    }
+    window.onpopstate = () => {
+      this.reroute();
+    };
+  }
 
-    reroute() {
-        const matchedRoute = this._routes.find(route => {
-            const matched = route.match(window.location.pathname);
-            return matched;
-        });
+  reroute() {
+    console.log("this._routes", this._routes);
 
-        matchedRoute.renderRoute();
-    }
+    const matchedRoute = this._routes.find((route) => {
+      const matched = route.match(window.location.pathname);
+      console.log("find return matched", matched);
+
+      return matched;
+    });
+
+    matchedRoute.renderRoute();
+    console.log("matchedRoute", matchedRoute);
+  }
 }
 
 const router = new Router(routes);
